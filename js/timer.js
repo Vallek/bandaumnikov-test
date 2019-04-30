400c2f53052923a9d5df8162d58fc6bda1e42fbc
Vallek <vslavinn@gmail.com>
2019-04-30 12:59:41 +0300

    Замена таймера на ваниальный js

--- js/timer.js
+++ js/timer.js
@@ -1,92 +1,44 @@
-Vue.component('Timer',{
-  template: `
-  <div>
-      <div class="hour time-wrapper">
-        <span class="number">{{ hours }}</span>
-        <div class="format">{{ wordString.hours }}</div>
-      </div>
-      <div class="min time-wrapper">
-        <span class="number">{{ minutes }}</span>
-        <div class="format">{{ wordString.minutes }}</div>
-      </div>
-      <div class="sec time-wrapper">
-        <span class="number">{{ seconds }}</span>
-        <div class="format">{{ wordString.seconds }}</div>
-      </div>
-    </div>
-  `,
-  props: ['starttime','endtime','trans'] ,
-  data: function(){
-  	return{
-    	timer:"",
-      wordString: {},
-      start: "",
-      end: "",
-      interval: "",
-      days: "",
-      minutes: "",
-      hours:"",
-      seconds:"",
-      message:"",
-      statusType:"",
-      statusText: "",
-    
-    };
-  },
-  created: function () {
-        this.wordString = JSON.parse(this.trans);
-    },
-  mounted(){
-    this.start = new Date(this.starttime).getTime();
-    this.end = new Date(this.endtime).getTime();
-    // Update the count down every 1 second
-    this.timerCount(this.start,this.end);
-    this.interval = setInterval(() => {
-        this.timerCount(this.start,this.end);
-    }, 1000);
-  },
-  methods: {
-    timerCount: function(start,end){
-        // Get todays date and time
-        var now = new Date().getTime();
+function countdown(dateEnd) {
+  var timer, days, hours, minutes, seconds;
 
-        // Find the distance between now an the count down date
-        var distance = start - now;
-        var passTime =  end - now;
+  dateEnd = new Date(dateEnd);
+  dateEnd = dateEnd.getTime();
 
-        if(distance < 0 && passTime < 0){
-            this.message = this.wordString.expired;
-            this.statusType = "expired";
-            this.statusText = this.wordString.status.expired;
-            clearInterval(this.interval);
-            return;
-
-        }else if(distance < 0 && passTime > 0){
-            this.calcTime(passTime);
-            this.message = this.wordString.running;
-            this.statusType = "running";
-            this.statusText = this.wordString.status.running;
-
-        } else if( distance > 0 && passTime > 0 ){
-            this.calcTime(distance); 
-            this.message = this.wordString.upcoming;
-            this.statusType = "upcoming";
-            this.statusText = this.wordString.status.upcoming;
-        }
-    },
-    calcTime: function(dist){
-      // Time calculations for days, hours, minutes and seconds
-        this.days = Math.floor(dist / (1000 * 60 * 60 * 24));
-        this.hours = Math.floor((dist % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        this.minutes = Math.floor((dist % (1000 * 60 * 60)) / (1000 * 60));
-        this.seconds = Math.floor((dist % (1000 * 60)) / 1000);
-    }
-    
+  if ( isNaN(dateEnd) ) {
+    return;
   }
-  
-});
 
-new Vue({
-  el: "#timer",
-});
+  timer = setInterval(calculate, 1000);
 
+  function calculate() {
+    var dateStart = new Date();
+    var dateStart = new Date(dateStart.getUTCFullYear(),
+      dateStart.getUTCMonth(),
+      dateStart.getUTCDate(),
+      dateStart.getUTCHours(),
+      dateStart.getUTCMinutes(),
+      dateStart.getUTCSeconds());
+    var timeRemaining = parseInt((dateEnd - dateStart.getTime()) / 1000)
+
+    if ( timeRemaining >= 0 ) {
+      days    = parseInt(timeRemaining / 86400);
+      timeRemaining   = (timeRemaining % 86400);
+      hours   = parseInt(timeRemaining / 3600);
+      timeRemaining   = (timeRemaining % 3600);
+      minutes = parseInt(timeRemaining / 60);
+      timeRemaining   = (timeRemaining % 60);
+      seconds = parseInt(timeRemaining);
+
+      // document.getElementById("days").innerHTML    = parseInt(days, 10);
+      document.getElementById("hours").innerHTML   = ("0" + hours).slice(-2);
+      document.getElementById("minutes").innerHTML = ("0" + minutes).slice(-2);
+      document.getElementById("seconds").innerHTML = ("0" + seconds).slice(-2);
+    } else {
+      return;
+    }
+  }
+
+  function display(days, hours, minutes, seconds) {}
+}
+
+countdown('05/10/2019 03:14:07 AM');
